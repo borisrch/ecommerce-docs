f746222152a4d3d9fbe08025c5d916eb6f9faaf3
Boris Chan <borisc1996@gmail.com>
2020-11-02 13:04:11 -0500

    add styles to ReactMarkdown component

--- pages/getting-started/overview.js
+++ pages/getting-started/overview.js
@@ -4,14 +4,52 @@ import Head from "../../src/components/head";
 
 import ReactMarkdown from "react-markdown";
 import matter from "gray-matter";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 
 const label = "Overview";
 
+// TODO: refactor to style from container
+const useStyles = makeStyles({
+  root: {
+    paddingLeft: "5%",
+    paddingRight: "5%",
+    color: "#333",
+    "& h1": {
+      color: "green",
+    },
+    "& img": {
+      width: "100%",
+      textAlign: "center",
+      padding: "5%",
+    },
+    "& code": {
+      backgroundColor: "#edf1fb",
+      color: "#1976d2",
+      padding: "4px 8px",
+      borderRadius: "4px",
+    },
+    "& p": {
+      lineHeight: "2.5em",
+      textAlign: "justify",
+    },
+  },
+});
+
 function Page({ content, data }) {
+  const classes = useStyles();
+
+  const ContentContainer = () => {
+    return (
+      <React.Fragment>
+        <ReactMarkdown source={content} className={classes.root} />
+      </React.Fragment>
+    );
+  };
+
   return (
     <React.Fragment>
       <Head title={label} />
-      <Container content={<ReactMarkdown source={content} />} label={label} />
+      <Container content={<ContentContainer />} label={label} />
     </React.Fragment>
   );
 }
