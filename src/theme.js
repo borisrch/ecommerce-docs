7434d40ddc1b52605f4c579c8f82836fd977c177
Boris Chan <borisc1996@gmail.com>
2020-11-02 15:54:57 -0500

    change font from Roboto to Noto Sans JP

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,9 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
-const theme = createMuiTheme({});
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: ["Noto Sans JP", "sans serif"].join(","),
+  },
+});
 
 export default theme;
